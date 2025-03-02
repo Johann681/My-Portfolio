4cdd3b2d14f2d787c6841d2da041178567610f11
johann681 <adekolaolaonipekun@gmail.com>
2025-03-02 19:20:38 +0100

    Hello

--- components/ui/3d-pin.tsx
+++ components/ui/3d-pin.tsx
@@ -70,15 +70,20 @@ export const PinPerspective = ({
       <div className=" w-full h-full -mt-7 flex-none  inset-0">
         <div className="absolute top-0 inset-x-0  flex justify-center">
           <div
-            href={href}
-            target="_blank"
-            className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 "
+            onClick={() => window.open(href, "_blank")}
+            className="cursor-pointer relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10"
           >
+            {/* Title/Text */}
             <div className="relative z-20 text-white text-xs font-bold inline-block py-0.5">
               {title}
             </div>
 
-            <div className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover/btn:opacity-40"></div>
+            {/* Gradient Underline Effect */}
+            <div
+              className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] 
+      bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 
+      transition-opacity duration-500 group-hover/btn:opacity-40"
+            ></div>
           </div>
         </div>
 
