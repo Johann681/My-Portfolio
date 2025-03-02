0e43ee233f68ae661cc9d811367ee043a02285d8
johann681 <adekolaolaonipekun@gmail.com>
2025-03-02 19:05:13 +0100

    made some changes

--- components/ui/3d-pin.tsx
+++ components/ui/3d-pin.tsx
@@ -69,7 +69,7 @@ export const PinPerspective = ({
     <motion.div className="pointer-events-none  w-full h-80 flex items-center justify-center opacity-0 group-hover/pin:opacity-100 z-[60] transition duration-500">
       <div className=" w-full h-full -mt-7 flex-none  inset-0">
         <div className="absolute top-0 inset-x-0  flex justify-center">
-          <span
+          <div
             href={href}
             target="_blank"
             className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 "
@@ -79,7 +79,7 @@ export const PinPerspective = ({
             </div>
 
             <div className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover/btn:opacity-40"></div>
-          </span>
+          </div>
         </div>
 
         <div
