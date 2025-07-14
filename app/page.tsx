8d470cbb58f5a0378e9dace3a3048cc1fdf718ef
𝑷𝒉𝒊𝒍𝒐𝒔𝒐𝒑𝒉𝒆𝒓 . <you@example.com>
2025-07-14 02:13:15 +0100

    avoid

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
 import Footer from "@/components/Footer";
@@ -6,20 +8,20 @@ import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { navItems } from "@/data";
-import Approach from "@/components/Approach"
+import Approach from "@/components/Approach";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col  mx-auto sm:px-10  overflow-clip">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 overflow-clip">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
-        <Clients/>
-        <Experience/>
-        <Approach/>
-        <Footer/>
+        <Clients />
+        <Experience />
+        <Approach />
+        <Footer />
       </div>
     </main>
   );
